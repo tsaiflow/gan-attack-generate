a6417d82b355af7f2a1ac0c7f77e06559de88971
Shuo <jimmyahacker@hotmail.com>
2018-05-09 14:29:08 -0400

    1.write utils functions in utils.py;2.modify ipynb to run and it runs

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import tensorflow as tf
 
 # We need two inputs, one for the discriminator and one for the generator. Here we'll call the discriminator input `inputs_real` and the generator input `inputs_z`. We'll assign them the appropriate sizes for each of the networks.
@@ -6,3 +7,16 @@ def model_inputs(real_dim, z_dim):
     inputs_z = tf.placeholder(tf.float32, (None, z_dim), name='input_z')
     
     return inputs_real, inputs_z
+
+def get_flow_dataset(filename=None):
+    if filename is None:
+        return np.random.normal(size=[20000, 40])
+    else:
+        pass
+
+def train_test_split(dataset, train_ratio=None, train_nums=None):
+    if train_ratio is not None:
+        train_nums = int(dataset.shape[0] * train_ratio)
+        return dataset[:train_nums], dataset[train_nums:]
+    else:
+        return dataset[:train_nums], dataset[train_nums:]
