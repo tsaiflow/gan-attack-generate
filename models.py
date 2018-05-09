8a8b562c11717b0362cedf3737be2eb10d5c0bda
Shuo <jimmyahacker@hotmail.com>
2018-05-09 13:56:21 -0400

    modify models:now both discriminator and generator has 2 hidden layers

--- models.py
+++ models.py
@@ -6,9 +6,13 @@ def generator(z, out_dim, n_units=128, reuse=False, alpha=0.01):
         h1 = tf.layers.dense(z, n_units, activation=None)
         # Leaky ReLU
         h1 = tf.maximum(alpha * h1, h1)
+        # Hidden layer
+        h2 = tf.layers.dense(h1, n_units, activation=None)
+        # Leaky ReLU
+        h2 = tf.maximum(alpha * h2, h2)
         
         # Logits and tanh output
-        logits = tf.layers.dense(h1, out_dim, activation=None)
+        logits = tf.layers.dense(h2, out_dim, activation=None)
         out = tf.tanh(logits)
         
         return out
@@ -19,8 +23,12 @@ def discriminator(x, n_units=128, reuse=False, alpha=0.01):
         h1 = tf.layers.dense(x, n_units, activation=None)
         # Leaky ReLU
         h1 = tf.maximum(alpha * h1, h1)
+        # Hidden layer
+        h2 = tf.layers.dense(h1, n_units, activation=None)
+        # Leaky ReLU
+        h2 = tf.maximum(alpha * h2, h2)
         
-        logits = tf.layers.dense(h1, 1, activation=None)
+        logits = tf.layers.dense(h2, 1, activation=None)
         out = tf.sigmoid(logits)
         
         return out, logits
