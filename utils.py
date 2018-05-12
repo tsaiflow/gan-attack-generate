f5672031cde139fd3887d458a24e3c05482b8039
Shuo <jimmyahacker@hotmail.com>
2018-05-12 15:20:15 -0400

    Done * use real data * sample randomly * select given features * select given benign and attack labels ToDo * Test the accuracy of the discriminator Note * the interface for hidden layer visualization is maintained, so not the concerns for now * normalization is not my concern for now

--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ def model_inputs(benign_dim, z_dim, attack_remains_dim):
     
     return inputs_benign, inputs_z, inputs_attack_remains
 
-def get_flow_dataset(filename=None, benign_label=0, attack_label=1):
+def get_flow_dataset(filename=None, benign_label=0, attack_label=1, sampling_rate=0.3):
     if filename is None:
         feature = np.random.uniform(size=[20000,40])
         label = np.random.randint(2, size=[20000,1])
@@ -18,7 +18,13 @@ def get_flow_dataset(filename=None, benign_label=0, attack_label=1):
     else:
         # TODO
         df = pd.read_csv(filename)
-        return df
+        RELEVANT_FEATURES = [' Source Port', ' Protocol', ' Flow Duration', ' Total Fwd Packets', 'Total Length of Fwd Packets', ' Total Length of Bwd Packets', ' Fwd Packet Length Max', ' Fwd Packet Length Min', 'Bwd Packet Length Max', ' Bwd Packet Length Mean', ' Bwd Packet Length Std', 'Flow Bytes/s', ' Flow Packets/s', ' Flow IAT Mean', ' Flow IAT Max', 'Fwd IAT Total', ' Fwd IAT Max', ' Fwd IAT Min', ' Bwd IAT Std', ' Bwd IAT Max', ' Bwd IAT Min', 'Fwd PSH Flags', ' Fwd URG Flags', ' Bwd URG Flags', ' Fwd Header Length', 'Fwd Packets/s', ' Min Packet Length', ' Max Packet Length', ' Packet Length Mean', 'FIN Flag Count', ' RST Flag Count', ' ACK Flag Count', ' CWE Flag Count', ' Down/Up Ratio', ' Average Packet Size', ' Avg Bwd Segment Size', 'Fwd Avg Bytes/Bulk', ' Fwd Avg Packets/Bulk', ' Fwd Avg Bulk Rate', ' Bwd Avg Bytes/Bulk', 'Bwd Avg Bulk Rate', 'Init_Win_bytes_forward', ' Init_Win_bytes_backward', ' act_data_pkt_fwd', ' Active Min', 'Idle Mean', ' Idle Std']
+        df = df[RELEVANT_FEATURES + [' Label']]
+        
+        benign, attack = df[df[' Label'] == benign_label][RELEVANT_FEATURES], df[df[' Label'] == attack_label][RELEVANT_FEATURES]
+        
+        benign, attack = benign.sample(frac=0.3), attack.sample(frac=0.3)
+        return benign.as_matrix(), attack.as_matrix()
     
 def get_same_len_benign_attack(benign, attack, shuffle=True):
     min_len = min(benign.shape[0], attack.shape[0])
