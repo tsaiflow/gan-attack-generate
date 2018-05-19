6652b03c60ffc825aa0bfe259ef17df408c2db04
Shuo <jimmyahacker@hotmail.com>
2018-05-18 20:59:46 -0400

    * work on rewrite structure * finish the generating dataset part

--- models.py
+++ models.py
@@ -16,6 +16,18 @@ def generator(z, out_dim, n_units, reuse=False, alpha=0.01):
         
         return out, layers_output[1:]
 
+class GeneratorModel(tf.keras.Model):
+  def __init__(self):
+    super(GeneratorModel, self).__init__()
+    self.dense1 = tf.keras.layers.Dense(units=128)
+    self.dense2 = tf.keras.layers.Dense(units=64)
+
+  def call(self, input):
+    """Run the model."""
+    result = self.dense1(input)
+    result = self.dense2(result)
+    return result
+
 def discriminator(x, n_units, reuse=False, alpha=0.01):
     with tf.variable_scope('discriminator', reuse=reuse):
         layers_output = [x] + [np.nan for _ in range(len(n_units) - 1)]
@@ -30,3 +42,15 @@ def discriminator(x, n_units, reuse=False, alpha=0.01):
         out = tf.sigmoid(logits)
         
         return out, logits, layers_output[1:]
+
+class DiscriminatorModel(tf.keras.Model):
+  def __init__(self):
+    super(GeneratorModel, self).__init__()
+    self.dense1 = tf.keras.layers.Dense(units=128)
+    self.dense2 = tf.keras.layers.Dense(units=64)
+
+  def call(self, input):
+    """Run the model."""
+    result = self.dense1(input)
+    result = self.dense2(result)
+    return result
