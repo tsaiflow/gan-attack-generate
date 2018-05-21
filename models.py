84dec1ca89c903803df90158bbdfcfb85c1d1ff1
Shuo <jimmyahacker@hotmail.com>
2018-05-20 21:47:47 -0400

    Done * remove redundant code in utils.py and models.py ToDo * as the previous commits show

--- models.py
+++ models.py
@@ -1,35 +1,5 @@
-import tensorflow as tf
 import numpy as np
-
-def generator(z, out_dim, n_units, reuse=False, alpha=0.01):
-    with tf.variable_scope('generator', reuse=reuse):
-        layers_output = [z] + [np.nan for _ in range(len(n_units) - 1)]
-        for i in range(1, len(n_units)):
-            # Hidden Layers
-            layers_output[i] = tf.layers.dense(layers_output[i - 1], n_units[i], activation=None)
-            # Leaky ReLU
-            layers_output[i] = tf.maximum(alpha * layers_output[i], layers_output[i])
-        
-        # Logits and sigmoid output
-        logits = tf.layers.dense(layers_output[-1], out_dim, activation=None)
-        out = tf.sigmoid(logits)
-        
-        return out, layers_output[1:]
-
-def discriminator(x, n_units, reuse=False, alpha=0.01):
-    with tf.variable_scope('discriminator', reuse=reuse):
-        layers_output = [x] + [np.nan for _ in range(len(n_units) - 1)]
-        for i in range(1, len(n_units)):
-            # Hidden Layers
-            layers_output[i] = tf.layers.dense(layers_output[i - 1], n_units[i], activation=None)
-            # Leaky ReLU
-            layers_output[i] = tf.maximum(alpha * layers_output[i], layers_output[i])
-        
-        # Logits and sigmoid output
-        logits = tf.layers.dense(layers_output[-1], 1, activation=None)
-        out = tf.sigmoid(logits)
-        
-        return out, logits, layers_output[1:]
+import tensorflow as tf
 
 class Generator(tf.keras.Model):
   def __init__(self, input_shape, output_shape=2):
--- utils.py
+++ utils.py
@@ -6,48 +6,6 @@ import tensorflow.contrib.eager as tfe
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import normalize
 
-def model_inputs(benign_dim, z_dim, attack_remains_dim):
-    inputs_benign = tf.placeholder(tf.float64, (None, benign_dim), name='input_benign') 
-    inputs_z = tf.placeholder(tf.float64, (None, z_dim), name='input_z')
-    inputs_attack_remains = tf.placeholder(tf.float64, (None, attack_remains_dim), name='input_attack_remains')
-    
-    return inputs_benign, inputs_z, inputs_attack_remains
-
-def get_flow_dataset(filename=None, benign_label=0, attack_label=1, train_frac=0.3):
-    if filename is None:
-        feature = np.random.uniform(size=[20000,40])
-        label = np.random.randint(2, size=[20000,1])
-        benign, attack = split_benign_attack(np.concatenate([feature,label], axis=1))
-        return get_same_len_benign_attack(benign, attack)
-    else:
-        # TODO
-        df = pd.read_csv(filename)
-        RELEVANT_FEATURES = [' Source Port', ' Destination Port', ' Flow Duration', 'Total Length of Fwd Packets', ' Total Length of Bwd Packets', 'Bwd Packet Length Max', ' Bwd Packet Length Min', 'Flow Bytes/s', ' Flow IAT Mean', ' Flow IAT Std', ' Flow IAT Max', ' Flow IAT Min', 'Fwd IAT Total', ' Fwd IAT Mean', ' Fwd IAT Std', ' Fwd IAT Min', 'Bwd IAT Total', ' Bwd IAT Mean', ' Bwd IAT Std', ' Bwd IAT Min', 'Fwd PSH Flags', ' Bwd PSH Flags', ' Fwd URG Flags', ' Fwd Header Length', ' Bwd Packets/s', ' Packet Length Mean', ' ACK Flag Count', ' Down/Up Ratio', ' Avg Fwd Segment Size', ' Fwd Header Length.1', 'Fwd Avg Bytes/Bulk', ' Fwd Avg Packets/Bulk', ' Bwd Avg Bytes/Bulk', 'Bwd Avg Bulk Rate', 'Subflow Fwd Packets', ' Subflow Fwd Bytes', 'Init_Win_bytes_forward', ' act_data_pkt_fwd', ' Active Std', ' Active Min', ' Idle Max']
-        df = df[RELEVANT_FEATURES + [' Label']]
-        
-        benign, attack = df[df[' Label'] == benign_label][RELEVANT_FEATURES], df[df[' Label'] == attack_label][RELEVANT_FEATURES]
-        
-        benign, attack = benign.as_matrix(), attack.as_matrix()
-        benign_train, benign_test = train_test_split(benign, train_size=train_frac)
-        attack_train, attack_test = train_test_split(attack, train_size=train_frac)
-        return (benign_train, benign_test), (attack_train, attack_test) 
-    
-def get_same_len_benign_attack(benign, attack, shuffle=True):
-    min_len = min(benign.shape[0], attack.shape[0])
-    if shuffle and shuffle is True:
-        np.random.shuffle(benign)
-        np.random.shuffle(attack)
-    return benign[:min_len], attack[:min_len]
-    
-def split_benign_attack(dataset):
-    benign, attack = [], []
-    for row in dataset:
-        if row[-1] == 0:
-            benign.append(row[:-1])
-        else:
-            attack.append(row[:-1])
-    return np.array(benign), np.array(attack)
-
 def max_norm(dataset):
     dataset = dataset - dataset.min(axis=0)
     dataset = normalize(dataset, axis=0, norm='max')
@@ -55,15 +13,6 @@ def max_norm(dataset):
 
 def parse_feature_label(row):
     return row[:-1], tf.cast(row[-1], tf.int32)
-
-def loss(model, x, y):
-    predict_y = model(x)
-    return tf.losses.sparse_softmax_cross_entropy(labels=y, logits=predict_y)
-
-def grad(model, inputs, targets):
-    with tf.GradientTape() as tape:
-        loss_value = loss(model, inputs, targets)
-    return tape.gradient(loss_value, model.variables)
 
 def sample_n_number(upper, n):
     return random.sample({i for i in range(upper)}, n)
