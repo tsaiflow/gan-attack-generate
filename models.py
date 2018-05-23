fe6989f33f95c50ca2efdb2d6ce2c3ff2e5a174d
Kaihua Cai <caikaihua@gmail.com>
2018-05-23 12:53:11 -0400

    changed model for detector & generator

--- models.py
+++ models.py
@@ -2,14 +2,17 @@ import numpy as np
 import tensorflow as tf
 
 tanh=tf.nn.tanh
+# kc added sigmoid
+sigmoid = tf.nn.sigmoid
 
 class Generator(tf.keras.Model):
   def __init__(self, input_shape, output_shape=2):
     super(Generator, self).__init__()
-    self.dense1 = tf.keras.layers.Dense(units=128, input_shape=(input_shape,), activation=tanh, dtype=tf.float64)
-    self.dense2 = tf.keras.layers.Dense(units=64, activation=tanh, dtype=tf.float64)
-    self.dense3 = tf.keras.layers.Dense(units=32, activation=tanh, dtype=tf.float64)
-    self.dense4 = tf.keras.layers.Dense(units=output_shape, activation=None, dtype=tf.float64)
+    self.dense1 = tf.keras.layers.Dense(units=32, input_shape=(input_shape,), activation=sigmoid, dtype=tf.float64)
+    self.dense2 = tf.keras.layers.Dense(units=16, activation=sigmoid, dtype=tf.float64)
+    self.dense3 = tf.keras.layers.Dense(units=8, activation=sigmoid, dtype=tf.float64)
+    # kc change to sigmoid
+    self.dense4 = tf.keras.layers.Dense(units=output_shape, activation=sigmoid, dtype=tf.float64)
 
   def call(self, inputs):
     """Run the model."""
@@ -22,13 +25,13 @@ class Generator(tf.keras.Model):
 class Discriminator(tf.keras.Model):
   def __init__(self):
     super(Discriminator, self).__init__()
-    self.dense1 = tf.keras.layers.Dense(units=32, activation=tanh, dtype=tf.float64)
-    self.dense2 = tf.keras.layers.Dense(units=16, activation=tanh, dtype=tf.float64)
-    self.dense3 = tf.keras.layers.Dense(units=1, activation=None, dtype=tf.float64)
+    self.dense1 = tf.keras.layers.Dense(units=16, activation=sigmoid, dtype=tf.float64)
+#     self.dense2 = tf.keras.layers.Dense(units=8, activation=tanh, dtype=tf.float64)
+    self.dense3 = tf.keras.layers.Dense(units=1, activation=sigmoid, dtype=tf.float64)
 
   def call(self, inputs):
     """Run the model."""
     result = self.dense1(inputs)
-    result = self.dense2(result)
+#     result = self.dense2(result)
     result = self.dense3(result)
     return result
